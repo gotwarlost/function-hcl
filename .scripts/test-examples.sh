4e8012165478855934a7844c91f7e8ac7b656bff
gotwarlost <krishnan.anantheswaran@elastic.co>
2025-08-17 13:04:42 -0700

    fix example tests to include context and results, with yaml doc fixer
    Signed-off-by: gotwarlost <krishnan.anantheswaran@elastic.co>

--- .scripts/test-examples.sh
+++ .scripts/test-examples.sh
@@ -4,24 +4,38 @@ set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-"${SCRIPT_DIR}/gen-comp.sh" >/dev/null
+filter=${1:-}
+"${SCRIPT_DIR}/gen-comp.sh" ${filter} >/dev/null
 echo ---
 
 cd  "${SCRIPT_DIR}/../example"
 
+run=0
 for dir in *
 do
   if [[ ! -d "${dir}" ]]
   then
     continue
   fi
+  if [[ "${filter}" != "" && "${filter}" != "${dir}" ]]
+  then
+    continue
+  fi
+  run=1
   echo $dir>&2
   cd $dir
-  cmd="crossplane render xr.yaml composition.yaml functions.yaml"
+  cmd="crossplane render -r -c xr.yaml composition.yaml functions.yaml"
   if [[ -f observed.yaml ]]
   then
     cmd="${cmd} -o observed.yaml"
   fi
-  dyff between --omit-header --additional-identifier type --set-exit-code src/expected.yaml  <(${cmd})
+  ${cmd} | "${SCRIPT_DIR}/fix-output-yaml.sh" >/tmp/results.yaml
+  dyff between -i --omit-header --additional-identifier type --set-exit-code src/expected.yaml  /tmp/results.yaml
   cd ..
 done
+
+if [[ "${run}" == "0" ]]
+then
+  echo "[ERROR] No directories matched filter $filter" >&2
+  exit 2
+fi
