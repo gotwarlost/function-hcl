be44198fa2b2df0453badd65952c43c8575439aa
gotwarlost <krishnan.anantheswaran@elastic.co>
2026-03-10 17:31:07 -0700

    fix formula and add workflow to register new tags
    Signed-off-by: gotwarlost <krishnan.anantheswaran@elastic.co>

--- Formula/fn-hcl-tools.rb
+++ Formula/fn-hcl-tools.rb
@@ -1,8 +1,10 @@
 class FnHclTools < Formula
   desc "CLI tools for function-hcl: format, analyze, and package HCL compositions"
   homepage "https://github.com/crossplane-contrib/function-hcl"
+  url "https://github.com/crossplane-contrib/function-hcl/archive/refs/tags/LATEST.tar.gz"
+  sha256 "PLACEHOLDER"
   license "Apache-2.0"
-  head "https://github.com/crossplane-contrib/function-hcl.git", branch: "docs"
+  head "https://github.com/crossplane-contrib/function-hcl.git", branch: "main"
 
   depends_on "go" => :build
 
